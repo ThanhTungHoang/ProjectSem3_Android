3f5b6aacb468f062af39f72a2d7a88c8c3035706
Thanh Tung Hoang <hoangthanhtung1404@gmail.com>
2022-04-20 18:47:56 +0700

    Add

--- app/src/main/java/com/example/energymonitoring/Fragment/homefragment.java
+++ app/src/main/java/com/example/energymonitoring/Fragment/homefragment.java
@@ -50,7 +50,7 @@ public class homefragment extends Fragment {
     private List<DailyEnergy>  dailyEnergyList;
     private TextView updateName;
     private Button Update, Cancel, btnDelete;
-    private Button Test;
+    private Button btnReturn;
     private View.OnClickListener mDisconnectListener;
     private FirebaseUser user;
     private String uid;
@@ -70,6 +70,7 @@ public class homefragment extends Fragment {
         /////////////////////
         itemsList = new ArrayList<>();
         dailyEnergyList = new ArrayList<>();
+        btnReturn = view.findViewById(R.id.btn_return);
 
         recyclerView = view.findViewById(R.id.recycleView);
 //        energyrecyclerView = view.findViewById(R.id.recycleView1);
@@ -150,16 +151,23 @@ public class homefragment extends Fragment {
 
             @Override
             public void seeMore(Items items) {
-//                DailyEnergy dailyEnergy = new DailyEnergy("14042001", "1", "2");
-//                dailyEnergyList.add(dailyEnergy);
+
                 Toast.makeText(getActivity(), "Click seeMore", Toast.LENGTH_SHORT).show();
                 final Dialog dialog = new Dialog(getContext());
                 dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
                 dialog.setContentView(R.layout.layout_dialog_view_history_energy);
                 Window window = dialog.getWindow();
-                window.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
+                window.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.MATCH_PARENT);
                 window.setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
                 dialog.setCancelable(true);
+                btnReturn = dialog.findViewById(R.id.btn_return);
+                btnReturn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        dialog.dismiss();
+                    }
+                });
+
 
 
                 dailyEnergyrecyclerView = dialog.findViewById(R.id.recycleView1);
@@ -179,7 +187,7 @@ public class homefragment extends Fragment {
                         Log.d("onChildAdded", String.valueOf(snapshot));
                         DailyEnergy dailyEnergy = snapshot.getValue(DailyEnergy.class);
                         dailyEnergyList.add(dailyEnergy);
-                        Toast.makeText(getActivity(), "onChildAdded", Toast.LENGTH_SHORT).show();
+//                        Toast.makeText(getActivity(), "onChildAdded", Toast.LENGTH_SHORT).show();
                         mrecycleViewDailyEnergyAdapter.notifyDataSetChanged();
 
                     }
